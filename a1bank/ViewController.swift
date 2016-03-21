a37764cba6b029e751ec243ace6e665ed215114b
Panagiotis Papasyamatis <peteshag@panagiotiss-mac.local>
2016-03-21 17:04:52 +1100

    Initial Commit

---
+++ a1bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  a1bank
+//
+//  Created by Panagiotis Papasyamatis on 21/03/2016.
+//  Copyright © 2016 Panagiotis Papastamatis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
