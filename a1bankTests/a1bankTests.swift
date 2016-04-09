6b25a7dfa786beaaa4345b79f9fb676d106c2541
P Z Papastamatis <petezpapa@gmail.com>
2016-04-09 21:37:39 +1000

    UI Tests

--- a1bankTests/a1bankTests.swift
+++ a1bankTests/a1bankTests.swift
@@ -13,7 +13,8 @@ class a1bankTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        continueAfterFailure = false
+        XCUIApplication().launch()
     }
     
     override func tearDown() {
@@ -22,8 +23,7 @@ class a1bankTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
     
     func testPerformanceExample() {
--- a1bankUITests/a1bankUITests.swift
+++ a1bankUITests/a1bankUITests.swift
@@ -2,7 +2,7 @@
 //  a1bankUITests.swift
 //  a1bankUITests
 //
-//  Created by Panagiotis Papasyamatis on 21/03/2016.
+//  Created by Panagiotis Papastamatis on 21/03/2016.
 //  Copyright © 2016 Panagiotis Papastamatis. All rights reserved.
 //
 
@@ -13,14 +13,8 @@ class a1bankUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
@@ -28,9 +22,77 @@ class a1bankUITests: XCTestCase {
         super.tearDown()
     }
     
+    func testHomeHasButtons()
+    {
+        let app = XCUIApplication()
+        XCTAssertGreaterThan(app.buttons.count, 0)
+    }
+    
+    func testHomeDoesNotHaveTooManyButtons()
+    {
+        let app = XCUIApplication()
+        XCTAssertEqual(app.buttons.count, 2)
+    }
+    
+    func testHomeHasLogo()
+    {
+        let app = XCUIApplication()
+        XCTAssert(app.images["homelogo"].exists)
+    }
+    
+    func testHomeHasCopyrightText()
+    {
+        let app = XCUIApplication()
+        XCTAssert(app.textFields["copyrighthome"].exists)
+    }
+    
+    func testLoginHasUsernameTextField()
+    {
+        let app = XCUIApplication()
+        app.buttons["loginButton"].tap()
+        let tf = app.textFields["Username"]
+        XCTAssert(tf.exists)
+    }
+    
+    func testLoginHasUsernameLabel()
+    {
+        let app = XCUIApplication()
+        app.buttons["loginButton"].tap()
+        let tf = app.staticTexts["Username"]
+        XCTAssert(tf.exists)
+    }
+    
+    func testLoginHasPasswordLabel()
+    {
+        let app = XCUIApplication()
+        app.buttons["loginButton"].tap()
+        let tf = app.staticTexts["Password"]
+        XCTAssert(tf.exists)
+    }
+    
+    func testAccountPageHasTableView()
+    {
+        let app = XCUIApplication()
+        app.buttons["loginButton"].tap()
+        
+        let usernameTextField = app.textFields["Username"]
+        usernameTextField.tap()
+        usernameTextField.typeText("user1")
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        XCUIDevice.sharedDevice().orientation = .Portrait
+        let element = app.childrenMatchingType(.Window).elementBoundByIndex(0).childrenMatchingType(.Other).element.childrenMatchingType(.Other).element
+        element.tap()
+        element.tap()
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("password")
+        app.buttons["Login"].tap()
+        XCTAssertTrue(app.tables.element.cells.elementBoundByIndex(2).exists)
+    }
+    
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
     }
     
 }
